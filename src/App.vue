9e41b0199df4cb1d8552480e98c603db6c847fb7
Marc <marc.touw@gmail.com>
2022-09-05 11:26:26 +0200

    parse settings as env variables

--- src/App.vue
+++ src/App.vue
@@ -39,8 +39,10 @@ export default {
   },
   mounted() {
     // Parse initialization
-    Parse.initialize(ParseSettings.parseAppId);
-    Parse.serverURL = ParseSettings.parseServerUrl;
+    // eslint-disable-next-line no-undef
+    Parse.initialize(process.env.PARSE_APP_ID || ParseSettings.parseAppId);
+    // eslint-disable-next-line no-undef
+    Parse.serverURL = process.env.PARSE_SERVER_URL || ParseSettings.parseServerUrl;
 
     this.currentUser = Parse.User.current();
   }
